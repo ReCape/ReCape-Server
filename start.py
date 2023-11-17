f70277094a03abb93fb27e6ebdb52ca231e90f4a
Boyne G <boynegregg312@gmail.com>
2023-11-16 22:03:08 -0400

    Potentially fixed bug where ReCape UUID reader would lock and fail

--- start.py
+++ start.py
@@ -2,15 +2,28 @@ import os
 import subprocess 
 import threading
 import optifine_server
+from contextlib import redirect_stdout, redirect_stderr
 
 DEBUG = os.path.exists(".debug")
 
 def mc_server():
-    subprocess.call("java -jar server.jar --nogui", shell=True)
+    if not DEBUG:
+        with open("log.java.txt", "w+") as log:
+            with redirect_stdout(log):
+                with redirect_stderr(log):
+                    subprocess.call("java -jar server.jar --nogui", shell=True)
+    else:
+        subprocess.call("java -jar server.jar --nogui", shell=True)
 
 def optifine_serve():
     server = optifine_server.Server(DEBUG)
-    server.start()
+    if not DEBUG:
+        with open("log.optifine.txt", "w+") as log:
+            with redirect_stdout(log):
+                with redirect_stderr(log):
+                    server.start()
+    else:
+        server.start()
 
 server_thread = threading.Thread(target=mc_server)
 optifine_thread = threading.Thread(target=optifine_serve)
@@ -22,7 +35,10 @@ if DEBUG:
     server = api.Server(DEBUG)
     server.start()
 else:
-    subprocess.run("python3 -m gunicorn --certfile=ssl/domain.cert.pem --keyfile=ssl/private.key.pem --bind 0.0.0.0:443 \"wsgi:create_server()\"", shell=True)
+    with open("log.api.txt", "w+") as log:
+            with redirect_stdout(log):
+                with redirect_stderr(log):
+                    subprocess.run("python3 -m gunicorn --certfile=ssl/domain.cert.pem --keyfile=ssl/private.key.pem --bind 0.0.0.0:443 \"wsgi:create_server()\"", shell=True)
 
 server_thread.join()
 optifine_thread.join()
--- uuids.py
+++ uuids.py
@@ -1,6 +1,8 @@
 import sqlite3
+import threading
 
 class UUIDs:
+    lock = threading.Lock()
     def __init__(self):
         self.con = sqlite3.connect("uuid.db", check_same_thread=False)
         self.cur = self.con.cursor()
@@ -8,32 +10,48 @@ class UUIDs:
         self.cur.execute("CREATE TABLE IF NOT EXISTS uuids (uuid varchar(255), username varchar(255))")
 
     def register(self, uuid, username):
-        self.cur.execute("DELETE FROM uuids WHERE uuid = ?", (uuid,))
-        self.con.commit()
-        self.cur.execute("DELETE FROM uuids WHERE username = ?", (username,))
-        self.con.commit()
-        
-        self.cur.execute("INSERT INTO uuids VALUES (?, ?)", (uuid, username))
-        self.con.commit()
+        try:
+            self.lock.acquire(True)
+            self.cur.execute("DELETE FROM uuids WHERE uuid = ?", (uuid,))
+            self.con.commit()
+            self.cur.execute("DELETE FROM uuids WHERE username = ?", (username,))
+            self.con.commit()
+            
+            self.cur.execute("INSERT INTO uuids VALUES (?, ?)", (uuid, username))
+            self.con.commit()
+        finally:
+            self.lock.release()
 
     def get_username(self, uuid):
-        self.cur.execute("SELECT * FROM uuids WHERE uuid = ?", (uuid,))
-        matching = self.cur.fetchall()
+        try:
+            self.lock.acquire(True)
+            self.cur.execute("SELECT * FROM uuids WHERE uuid = ?", (uuid,))
+            matching = self.cur.fetchall()
+        finally:
+            self.lock.release()
         
         if len(matching) > 0:
             return matching[0][1]
         return False
     
     def get_uuid(self, username):
-        self.cur.execute("SELECT * FROM uuids WHERE username = ?", (username,))
-        matching = self.cur.fetchall()
+        try:
+            self.lock.acquire(True)
+            self.cur.execute("SELECT * FROM uuids WHERE username = ?", (username,))
+            matching = self.cur.fetchall()
+        finally:
+            self.lock.release()
         
         if len(matching) > 0:
             return matching[0][0]
         return False
     
     def get_user_count(self):
-        self.cur.execute("SELECT * FROM uuids")
+        try:
+            self.lock.acquire(True)
+            self.cur.execute("SELECT * FROM uuids")
+        finally:
+            self.lock.release()
         return len(self.cur.fetchall())
     
     def finish(self):
